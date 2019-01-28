4c1e088e0e1d731abda19c1fe90a932090ac74f9
Radik Shaymardanov <kaddafi88@gmail.com>
2019-01-29 01:40:25 +0500

    Исправлено согласно замечаниям.
    Добавлена поддержка IE11

--- js/script.js
+++ js/script.js
@@ -1,49 +1,54 @@
-let calMonth = document.querySelector('.calendar__month_list'),
-    listMonth = calMonth.getElementsByTagName('li'),
-    data = document.getElementById('data');
-    // создаем переменные для взаимодействия со страницей
+'use strict';
 
-let servObj,
-    servElem;
-    // переменные для получения содержимого с сервера
+// создаем переменные для взаимодействия со страницей
+var calMonth = document.querySelector('.calendar__month_list'),
+    calData = document.querySelector('.calendar__data'),
+    listMonth = document.querySelectorAll('.calendar__month_list-elem'),
+    data = document.getElementById('data');
+
+// переменные для получения содержимого с сервера
+var servObj = void 0,
+    servElem = void 0;
 
 // вызываем обработчик событий
 calMonth.addEventListener('click', function (e) {
 
-  let element = e.target.className,
-      elemValue = e.target;    
-  
-  writeData(element, elemValue);
+  var elemValue = e.target;
 
-})
-
-function writeData(element, elemValue) {
-
-  if (elemValue.tagName == 'LI') {
-    servElem = servObj[element].events.map(item => item);
-    data.value = '';  
-    for (let i = 0; i < servElem.length; i++) {
-      data.value += `${servElem[i].date} `;
-      data.value += `${servElem[i].event} \n`;    
-    }   
+  // проверка номера элемента в массиве на который нажали
+  for (var i = 0; i < listMonth.length; i++) {
+    if (listMonth[i] === elemValue) {
+      writeData(i);
+    }
   }
-  
+});
+
+function writeData(element) {
+
+  servElem = servObj[element].events.map(function (item) {
+    return item;
+  });
+  var mass = '';
+  for (var i = 0; i < servElem.length; i++) {
+    mass = mass + ' ' + servElem[i].date + ' ' + servElem[i].event + ' <br>';
+    calData.innerHTML = mass;
+  }
 }
 
 // функция запроса данных с сервера
 function ajax_get(url, callback) {
   var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function() {
-      if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-          console.log('responseText:' + xmlhttp.responseText);
-          try {
-              var data = JSON.parse(xmlhttp.responseText);
-          } catch(err) {
-              console.log(err.message + " in " + xmlhttp.responseText);
-              return;
-          }
-          callback(data);
+  xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+      // console.log('responseText:' + xmlhttp.responseText);
+      try {
+        var data = JSON.parse(xmlhttp.responseText);
+      } catch (err) {
+        console.log(err.message + " in " + xmlhttp.responseText);
+        return;
       }
+      callback(data);
+    }
   };
 
   xmlhttp.open("GET", url, true);
@@ -51,10 +56,6 @@ function ajax_get(url, callback) {
 }
 
 // получение данных и перекладка в переменную
-ajax_get('../data.json', function(data) {  
+ajax_get('../data.json', function (data) {
   servObj = data;
 });
-
-
-
-
